3ca607ddcadde9d8ecc718d3307b5bf14f506a18
kangruixiang <kangruixiang@gmail.com>
2026-01-11 16:14:13 -0500

    added versions

--- curator/src/lib/components/Settings/Version.svelte
+++ curator/src/lib/components/Settings/Version.svelte
@@ -4,6 +4,7 @@
 
 <div class="card">
 	<div class="gap-golden-md flex items-center justify-end">
+		Version {import.meta.env.APP_VERSION}
 		<div>
 			<a href="https://curator.krxiang.com"><Globe /></a>
 		</div>
--- curator/vite.config.ts
+++ curator/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
+
+const json = readFileSync(new URL('../package.json', import.meta.url), 'utf-8');
+const pkg = JSON.parse(json);
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.APP_VERSION': JSON.stringify(pkg.version)
+	}
 });
