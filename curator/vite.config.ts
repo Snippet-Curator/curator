863d976fb1939ec5358cc11bbe7fc10bb116c9fc
kangruixiang <kangruixiang@gmail.com>
2026-01-11 21:13:21 -0500

    udpated packaging

--- curator/vite.config.ts
+++ curator/vite.config.ts
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { readFileSync } from 'fs';
 
-const json = readFileSync(new URL('../package.json', import.meta.url), 'utf-8');
+const json = readFileSync(new URL('./package.json', import.meta.url), 'utf-8');
 const pkg = JSON.parse(json);
 
 export default defineConfig({
