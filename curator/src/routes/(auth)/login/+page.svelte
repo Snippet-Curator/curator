22b3969a5ff87436bc0d2b0498b6b8e2cff28079
kangruixiang <kangruixiang@gmail.com>
2026-01-13 17:00:24 -0500

    added login/signup pages

---
+++ curator/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Icon from '$lib/components/Layout/Icon.svelte';
+	import { ArrowBigRight } from 'lucide-svelte';
+</script>
+
+<section class="bg-base-100 flex min-h-screen w-full items-center justify-center">
+	<div
+		class=" card border-base-content/20 bg-base-100 flex w-lg flex-col items-center border py-10"
+	>
+		<div class="space-x-golden-sm flex">
+			<Icon /> <span class="text-3xl font-semibold select-none">Curator</span>
+		</div>
+		<div>
+			<fieldset class="fieldset rounded-box w-xs p-4">
+				<label class="fieldset-legend">Email</label>
+				<input type="email" class="input" placeholder="Email" />
+
+				<label class="fieldset-legend">Password</label>
+				<input type="password" class="input" placeholder="Password" />
+
+				<button class="btn btn-neutral mt-4">Login</button>
+
+				<a class="link mt-2 text-right" href="/signup"> Sign Up </a>
+			</fieldset>
+		</div>
+	</div>
+</section>
---
+++ curator/src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Icon from '$lib/components/Layout/Icon.svelte';
+</script>
+
+<section class="bg-base-100 flex min-h-screen w-full items-center justify-center">
+	<div
+		class=" card border-base-content/20 bg-base-100 flex w-lg flex-col items-center border py-10"
+	>
+		<div class="space-x-golden-sm flex">
+			<Icon /> <span class="text-3xl font-semibold select-none">Curator</span>
+		</div>
+		<div>
+			<fieldset class="fieldset rounded-box w-xs p-4">
+				<label class="fieldset-legend">Email</label>
+				<input type="email" class="input" placeholder="Email" />
+
+				<label class="fieldset-legend">Password</label>
+				<input type="password" class="input" placeholder="Password" />
+
+				<button class="btn btn-neutral mt-4">Sign Up</button>
+
+				<a class="link mt-2 text-right" href="/login"> Login </a>
+			</fieldset>
+		</div>
+	</div>
+</section>
