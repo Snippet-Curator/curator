82efd68288de0a4c509429b2114526c68706872a
kangruixiang <kangruixiang@gmail.com>
2026-08-31 23:29:26 -0400

    upgrade version

--- curator/vite.config.ts
+++ curator/vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { readFileSync } from 'fs';
+import tailwindcss from '@tailwindcss/vite';
 
 const json = readFileSync(new URL('./package.json', import.meta.url), 'utf-8');
 const pkg = JSON.parse(json);
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		fs: {
 			allow: ['..']
